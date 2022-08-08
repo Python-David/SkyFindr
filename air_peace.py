5e2aae0a88cc796ecebd2abffc04ccea5bcb5306
kingbarz <mrnwobodo@gmail.com>
2022-08-08 15:54:50 +0100

    second commit: solved no flights issue, sold out issue and handled exceptions

--- air_peace.py
+++ air_peace.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
 air_peace_url = "https://www.flyairpeace.com/"
 chrome_driver_path = "/Users/kingbarz/Documents/Development/chromedriver"
-wait_time = 60 * 60
+wait_time = 60
 
 
 class AirPeace:
@@ -18,65 +21,80 @@ class AirPeace:
         self.driver.get(air_peace_url)
 
     def get_today_flight_details(self, origin, destination):
-        # 1. Handle Pop-up
-        pop_up_close = self.driver.find_element(By.XPATH, "/html/body/header/div[3]/div[1]/div/div/div/a[1]/button")
-        time.sleep(3)
-        pop_up_close.click()
+        try:
+            # 1. Handle Pop-up
+            pop_up_close = self.driver.find_element(By.XPATH, "/html/body/header/div[3]/div[1]/div/div/div/a[1]/button")
+            pop_up_close.click()
 
-        # 2a. Form Interaction
-        # 2b. Switch to frame
-        self.driver.switch_to.frame(self.driver.find_element(By.ID, "reservation_form"))
+            # 2a. Form Interaction
+            # 2b. Switch to frame
+            self.driver.switch_to.frame(self.driver.find_element(By.ID, "reservation_form"))
 
-        # 3. Select Origin (frame in element)
-        from_dropdown = self.driver.find_element(By.NAME, "Origin")
-        fdd = Select(from_dropdown)
-        fdd.select_by_value(origin)
+            # 3. Select Origin (frame in element)
+            from_dropdown = self.driver.find_element(By.NAME, "Origin")
+            fdd = Select(from_dropdown)
+            fdd.select_by_value(origin)
 
-        # 4. Select Destination
-        to_dropdown = self.driver.find_element(By.NAME, "Destination")
-        tdd = Select(to_dropdown)
-        tdd.select_by_value(destination)
+            # 4. Select Destination
+            to_dropdown = self.driver.find_element(By.NAME, "Destination")
+            tdd = Select(to_dropdown)
+            tdd.select_by_value(destination)
 
-        # 5. Select ticket type
-        ticket_type = self.driver.find_element(By.XPATH,
-                                               "/html/body/form/div[5]/div/div[1]/div[3]/div/div/div/div/label[2]")
-        ticket_type.click()
+            # 5. Select ticket type
+            ticket_type = self.driver.find_element(By.XPATH,
+                                                   "/html/body/form/div[5]/div/div[1]/div[3]/div/div/div/div/label[2]")
+            ticket_type.click()
 
-        # 6. Submit
-        submit = self.driver.find_element(By.ID, "submitButton")
-        time.sleep(3)
-        submit.click()
+            # 6. Submit
+            submit = self.driver.find_element(By.ID, "submitButton")
+            time.sleep(3)
+            submit.click()
 
-        # 7. Switch back to main page(back one frame)
+            # 7. Switch back to main page(back one frame)
 
-        time.sleep(10)
-        self.driver.switch_to.default_content()
+            time.sleep(10)
+            self.driver.switch_to.default_content()
 
-        # 8. Get all flight details
+            # 8. Get all flight details
 
-        flight_rows = self.driver.find_elements(By.XPATH, '//div[@class="panel-heading flt-panel-heading "]')
+            # wait = WebDriverWait(self.driver, 10)
+            # wait.until(EC.visibility_of_element_located(By.XPATH('//div[@class="panel-heading flt-panel-heading "]')))
 
-        price_list = []
-        time_list = []
+            flight_rows = self.driver.find_elements(By.XPATH, '//div[@class="panel-heading flt-panel-heading "]')
 
-        for WebElement in flight_rows:
-            element_html = WebElement.get_attribute("outerHTML")
-            element_soup = BeautifulSoup(element_html, "html.parser")
+            price_list = []
+            time_list = []
 
-            # Price
+            for WebElement in flight_rows:
+                element_html = WebElement.get_attribute("outerHTML")
+                element_soup = BeautifulSoup(element_html, "html.parser")
 
-            price = element_soup.find("div", {"class": "col-lg-12 fare-price-small"}).text.replace("\n", "")
-            price_list.append(price)
+                # Price
 
-            # Departure and arrival times
+                price = element_soup.find("div", {"class": "col-lg-12 fare-price-small"})
+                if price is None:
+                    pass
+                    price_list.append("SOLD OUT")
+                else:
+                    price_list.append(price.text.replace("\n", ""))
+                # print(price)
+                # price_list.append(price)
 
-            time_of_flight = element_soup.find_all("span", {"class": "time"})
-            flight_time = [x.text for x in time_of_flight]
-            time_list.append(flight_time)
+                # Departure and arrival times
 
-        flight_info = {}
+                time_of_flight = element_soup.find_all("span", {"class": "time"})
+                flight_time = [x.text for x in time_of_flight]
+                time_list.append(flight_time)
 
-        for num in range(len(price_list)):
-            flight_info[num] = price_list[num], time_list[num]
+            flight_info = {}
 
-        print(flight_info)
+            for num in range(len(price_list)):
+                flight_info[num] = price_list[num], time_list[num]
+
+            if len(flight_info) > 0:
+                print(flight_info)
+            else:
+                print("Sorry, no flights found.")
+
+        except NoSuchElementException:
+            print("Sorry, cannot return flight details for this destination.")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from air_peace import AirPeace
 
 air_peace = AirPeace()
-air_peace.get_today_flight_details("ABV", "LOS")
+air_peace.get_today_flight_details("LOS", "ABV")
